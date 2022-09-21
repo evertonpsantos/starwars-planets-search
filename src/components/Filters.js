a8b106194251b5d78ec8e5c0620050ed5e2b9937
Everton Pires dos Santos <evertonpsantos8@gmail.com>
2022-09-20 21:37:37 -0300

    Requisitos 3 e 4 :ringed_planet:

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,12 +1,26 @@
 import React, { useContext, useEffect, useState } from 'react';
 import planetsContext from '../context/PlanetsContext';
 
+const INIT_STATE = {
+  columnFilter: 'population',
+  comparisonFilter: 'maior que',
+  valueFilter: 0,
+};
+
 export default function Filters() {
   const [inputText, setName] = useState('');
-  const { filterPlanetsByName } = useContext(planetsContext);
+  const [numericFilters, setFilters] = useState(INIT_STATE);
+  const { filterPlanetsByName, filterByNumbers } = useContext(planetsContext);
 
-  const handleChange = ({ target }) => {
+  const handleTextChange = ({ target }) => {
     setName(target.value);
+  };
+
+  const handleNumericValues = ({ target }) => {
+    setFilters((prevState) => ({
+      ...prevState,
+      [target.name]: target.value,
+    }));
   };
 
   useEffect(() => {
@@ -14,16 +28,58 @@ export default function Filters() {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [inputText]);
 
+  const submitValues = () => {
+    filterByNumbers(numericFilters);
+  };
+
   return (
     <section>
       <input
         type="text"
         data-testid="name-filter"
-        onChange={ handleChange }
+        onChange={ handleTextChange }
         value={ inputText }
         id="name-filter"
         placeholder="Procure pelo nome"
       />
+
+      <select
+        data-testid="column-filter"
+        onChange={ handleNumericValues }
+        name="columnFilter"
+      >
+        <option value="population">population</option>
+        <option value="orbital_period">orbital_period</option>
+        <option value="diameter">diameter</option>
+        <option value="rotation_period">rotation_period</option>
+        <option value="surface_water">surface_water</option>
+      </select>
+
+      <select
+        data-testid="comparison-filter"
+        onChange={ handleNumericValues }
+        name="comparisonFilter"
+      >
+        <option value="maior que">maior que</option>
+        <option value="menor que">menor que</option>
+        <option value="igual a">igual a</option>
+      </select>
+
+      <input
+        type="number"
+        data-testid="value-filter"
+        onChange={ handleNumericValues }
+        name="valueFilter"
+        value={ numericFilters.valueFilter }
+      />
+
+      <button
+        type="button"
+        data-testid="button-filter"
+        onClick={ submitValues }
+      >
+        Filtrar
+      </button>
     </section>
   );
 }
--- src/context/PlanetsProvider.js
+++ src/context/PlanetsProvider.js
@@ -8,6 +8,7 @@ export default function PlanetsProvider({ children }) {
   const [planets, setPlanets] = useState([]);
   const [filteredList, setFilteredList] = useState(planets);
   const [filterByName, setNameFilter] = useState({});
+  const [filterByNumericValues, setNumericFilter] = useState([]);
 
   useEffect(() => {
     const getPlanets = async () => {
@@ -27,6 +28,27 @@ export default function PlanetsProvider({ children }) {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [filterByName]);
 
+  useEffect(() => {
+    filterByNumericValues.forEach(({ column, comparison, value }) => {
+      if (comparison === 'maior que') {
+        const filteredByNumbers = filteredList
+          .filter((planet) => planet[column] > Number(value));
+        return setFilteredList(filteredByNumbers);
+      }
+      if (comparison === 'menor que') {
+        const filteredByNumbers = filteredList
+          .filter((planet) => planet[column] < Number(value));
+        return setFilteredList(filteredByNumbers);
+      }
+      if (comparison === 'igual a') {
+        const filteredByNumbers = filteredList
+          .filter((planet) => Number(planet[column]) === Number(value));
+        return setFilteredList(filteredByNumbers);
+      }
+    });
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [filterByNumericValues]);
+
   const filterPlanetsByName = (inputValue) => {
     const nameInput = {
       name: inputValue,
@@ -34,10 +56,21 @@ export default function PlanetsProvider({ children }) {
     setNameFilter(nameInput);
   };
 
+  const filterByNumbers = ({ columnFilter, comparisonFilter, valueFilter }) => {
+    const filterValues = {
+      column: columnFilter,
+      comparison: comparisonFilter,
+      value: valueFilter,
+    };
+
+    setNumericFilter((prevFilters) => [...prevFilters, filterValues]);
+  };
+
   const contextValue = {
     planets,
     filteredList,
     filterPlanetsByName,
+    filterByNumbers,
   };
 
   return (
